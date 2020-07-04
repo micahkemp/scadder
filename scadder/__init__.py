ef61553812f0a1113e4630b4638e126c5e7bd84b
Micah Kemp <apple@micah.ws>
2020-07-03 22:06:20 -0500

    Added linting.

--- scadder/__init__.py
+++ scadder/__init__.py
@@ -1,1 +1,5 @@
+"""
+Python module to template OpenSCAD files with less frustration.
+"""
+
 __version__ = "0.0.1"
--- scadder/scadvariable.py
+++ scadder/scadvariable.py
@@ -1,11 +1,28 @@
-class SCADVariable(object):
-    def __init__(self, default=None, required=False):
+"""
+Components related to variables passed to OpenSCAD modules
+"""
+
+class SCADVariable:
+    """
+    Represents a variable passed to an OpenSCAD module
+    """
+    def __init__(self, default=None, required=False, validator=Validators.validate_true):
+        """
+        :param default: Default value for this variable.  Defaults to None.
+        :param required: Is this variable required to be explicitly set?  Defaults to False.
+        :param validator: The function to determine if a passed value is valid.
+        """
         self._default = default
         self._is_required = required
         self._is_set = False
+        self._validator = validator
 
     @property
     def value(self):
+        """
+        Get the value set.  If this variable is required, and no default is configured, raises
+        :class:`SCADMissingRequiredValue`.
+        """
         if self._is_required and not self._is_set:
             raise SCADMissingRequiredValue
 
@@ -16,6 +33,39 @@ class SCADVariable(object):
         self._value = value
         self._is_set = True
 
+    def validate_value(self, value):
+        """
+        :param value: The value to be validated.
+        :return: The result of the ``validator`` function.
+        """
+        return self._validator(value)
+
+
+class Validators:
+    """
+    Collection of validator functions for common uses.
+    """
+    @staticmethod
+    def validate_true(value):
+        """
+        Always returns True
+        :param value: The value being validated
+        :return: True
+        """
+        return value or True
+
+    @staticmethod
+    def validate_numeric(value):
+        """
+        Validates the passed value is numeric
+        :param value: The value being validated
+        :return: True if :param value: is numeric
+        """
+        return isinstance(value, int)
+
 
 class SCADMissingRequiredValue(Exception):
-    pass
+    """
+    Raised when :class:`SCADVariable` instance is asked for its value when there is none,
+    and the instance is defined as required.
+    """
