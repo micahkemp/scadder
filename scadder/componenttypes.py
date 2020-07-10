5aef7ae22f2dce7b921e55ac69fe39458c4c84c2
Micah Kemp <apple@micah.ws>
2020-07-10 16:07:23 -0500

    Added some ComponentWithChildren types.

--- scadder/componenttypes.py
+++ scadder/componenttypes.py
@@ -1,7 +1,7 @@
 """
 Collection of Component implementations
 """
-from .component import Component
+from .component import Component, ComponentWithChildren
 from .parameter import Parameter
 from .validators import Validators
 
@@ -22,3 +22,42 @@ class Cylinder(Component):
     radius = Parameter(validator=Validators.validate_numeric, template_as="r")
     diameter = Parameter(validator=Validators.validate_numeric, template_as="d")
     height = Parameter(validator=Validators.validate_numeric, template_as="h")
+
+
+class Translate(ComponentWithChildren):
+    """
+    Translate
+    """
+    _call_module = "translate"
+    vector = Parameter(
+        required=True,
+        validator=Validators.validate_list_numeric,
+        template_as="v"
+    )
+
+
+class Union(ComponentWithChildren):
+    """
+    Union
+    """
+    _call_module = "union"
+
+
+class Difference(ComponentWithChildren):
+    """
+    Difference
+    """
+    _call_module = "difference"
+
+
+class Mirror(ComponentWithChildren):
+    """
+    Mirror
+    """
+    _call_module = "mirror"
+
+    vector = Parameter(
+        required=True,
+        validator=Validators.validate_list_numeric,
+        template_as="v"
+    )
