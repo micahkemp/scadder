36aedab4139650a8694ef1178b965569a92609b6
Micah Kemp <apple@micah.ws>
2020-07-18 12:49:00 -0500

    Added docstrings.

--- scadder/validate.py
+++ scadder/validate.py
@@ -1,9 +1,20 @@
+"""
+Convenience argument validation
+"""
 from numbers import Number
 
 
 class Validator:
+    """
+    Base class for validators
+    """
     @classmethod
     def validate(cls, value):
+        """
+        Perform validation of passed value
+        :param value: The value to be validated
+        :return: If valid, returns the passed value.  If invalid, raises InvalidValue
+        """
         if not cls.is_valid(value):
             raise InvalidValue
 
@@ -11,24 +22,45 @@ class Validator:
 
     @staticmethod
     def is_valid(value):
-        raise NotImplemented
+        """
+        Override this function in subclasses.  Raises NotImplementedError otherwise.
+        :param value: The value to be validated
+        :return: True if valid, otherwise False
+        """
+        raise NotImplementedError
 
 
 class ValidateNumeric(Validator):
+    """
+    Validator for numeric values
+    """
     @staticmethod
     def is_valid(value):
+        """
+        :param value: The value to be validated
+        :return: True if numeric, otherwise False
+        """
         if isinstance(value, Number):
             return True
         return False
 
 
 class ValidateString(Validator):
+    """
+    Validator for string values
+    """
     @staticmethod
     def is_valid(value):
+        """
+        :param value: The value to be validated
+        :return: True if a string, otherwise False
+        """
         if isinstance(value, str):
             return True
         return False
 
 
 class InvalidValue(Exception):
-    pass
+    """
+    Raised when validation of a value failed
+    """
