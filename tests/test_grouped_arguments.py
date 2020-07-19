cd156d86563f5d98f0a276e18aeb7a3d082c0319
Micah Kemp <apple@micah.ws>
2020-07-18 20:49:14 -0500

    Added gruopedarguments.

---
+++ tests/test_grouped_arguments.py
@@ -0,0 +1,6 @@
+from scadder.groupedarguments import XYZ
+
+def test_xyz_angle_degrees():
+    # simple tests to ensure we ordered the X/Y correctly for the atan2 function
+    assert XYZ(1, 1, 0).angle_xy_deg == 45
+    assert XYZ(1, -1, 0).angle_xy_deg == -45
